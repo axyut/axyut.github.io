9965ea07376719f94970f193abae509cfa4d2252
axyut <aoogleak@gmail.com>
2024-12-01 21:43:43 +0545

    feat: new port with astro base

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://achyutkoirala.com.np', // Required for sitemap -> Replace with your site's URL
+	output: 'hybrid',
+	integrations: [tailwind(), sitemap()],
+	adapter: vercel(),
+});
