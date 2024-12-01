1eebfcae8f71b5e722896006045a5a5c1be39f85
axyut <aoogleak@gmail.com>
2024-12-01 22:33:49 +0545

    fix: build and deploy config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import { defineConfig } from 'astro/config';
+// import sitemap from '@astrojs/sitemap';
+// import tailwind from '@astrojs/tailwind';
+// import vercel from '@astrojs/vercel/serverless';
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://achyutkoirala.com.np', // Required for sitemap -> Replace with your site's URL
+// 	output: 'hybrid',
+// 	integrations: [tailwind(), sitemap()],
+// 	adapter: vercel(),
+// });
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://achyutkoirala.com.np', // Required for sitemap -> Replace with your site's URL
-	output: 'hybrid',
-	integrations: [tailwind(), sitemap()],
-	adapter: vercel(),
-});
+  site: "https://achyutkoirala.com.np",
+  output: "static",
+  integrations: [tailwind()],
+})
