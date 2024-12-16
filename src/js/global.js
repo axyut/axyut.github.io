37b2e7863696849cfa7e17cf56b9a54f2640e692
axyut <axyut.me@gmail.com>
2024-12-16 15:38:58 +0545

    feat: matrix buttoned

--- src/js/global.js
+++ src/js/global.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const resumeButton = document.getElementById("manpage-activator")
   const manualContainer = document.getElementById("container")
   const topSection = document.getElementById("top-section")
+  const matrixer = document.getElementById("matrixer")
+  const svg_background = document.getElementById("svg-background")
+  svg_background.classList.add("hidden")
 
   if (!manualContainer.classList.contains("hidden")) {
     manualContainer.classList.add("hidden")
@@ -20,5 +23,17 @@ document.addEventListener("DOMContentLoaded", () => {
     topSection.classList.add("hidden")
     manualContainer.classList.remove("hidden")
     manualContainer.classList.add("show")
+  })
+
+  matrixer.addEventListener("click", (e) => {
+    if (svg_background.classList.contains("hidden")) {
+      svg_background.classList.remove("hidden")
+      matrixer.innerHTML = "matrix!"
+      return
+    } else {
+      svg_background.classList.add("hidden")
+      matrixer.innerHTML = "matrix?"
+      return
+    }
   })
 })
